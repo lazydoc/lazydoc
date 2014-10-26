d9f82d6396a57a64c9102e28a43b3155f51b5888
Thongol <marc.eckart@gmail.com>
2014-10-26 16:31:02 +0100

    Added projectclasspath to plugin

--- lazydoc-plugin/src/main/java/org/lazydoc/plugin/LazyDocMojo.java
+++ lazydoc-plugin/src/main/java/org/lazydoc/plugin/LazyDocMojo.java
@@ -16,13 +16,26 @@ package org.lazydoc.plugin;
  * limitations under the License.
  */
 
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.lazydoc.config.Config;
 import org.lazydoc.parser.DocumentationParser;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Mojo(name = "generate-apidoc", defaultPhase = LifecyclePhase.COMPILE, executionStrategy = "always")
 @Execute(goal = "generate-apidoc", phase = LifecyclePhase.COMPILE)
@@ -32,8 +45,8 @@ public class LazyDocMojo extends AbstractMojo {
     @Parameter
     private Config config;
 
-    @Component
-    private PlexusConfig
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -45,6 +58,29 @@ public class LazyDocMojo extends AbstractMojo {
         } catch (Exception e) {
             getLog().error("Error parsing for documentation.", e);
             throw new MojoFailureException("Error parsing for documentation."+e.getMessage());
+        }
+    }
+
+    private void addProjectClasspathToPlugin() {
+        try {
+            Set<URL> urls = new HashSet<>();
+            List<String> elements = project.getCompileClasspathElements();
+            //getRuntimeClasspathElements()
+            //getCompileClasspathElements()
+            //getSystemClasspathElements()
+            for (String element : elements) {
+                urls.add(new File(element).toURI().toURL());
+            }
+
+            ClassLoader contextClassLoader = URLClassLoader.newInstance(
+                    urls.toArray(new URL[0]),
+                    Thread.currentThread().getContextClassLoader());
+
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+        } catch (DependencyResolutionRequiredException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
